fc8808b4909fa307fab08a0b85ea93e2c807f530
Daniel Ribeiro <danielribeiro.new@gmail.com>
2025-07-12 11:21:52 -0300

    simplified resource path func

--- main.py
+++ main.py
@@ -6,13 +6,10 @@ import subprocess
 import platform
 import time
 import os
-import sys
+import pathlib
 
 def resource_path(relative_path):
-    if hasattr(sys, '_MEIPASS'):
-        return os.path.join(sys._MEIPASS, relative_path)
-    else:
-        return os.path.join(os.path.dirname(os.path.abspath(__file__)), relative_path)
+    return str(pathlib.Path(__file__).parent / relative_path)
 
 def run_command(command):
     try:
